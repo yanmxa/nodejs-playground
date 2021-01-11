dbef55cd660285c96113a4107db7a49e2a134230
Yanmeng <ymeng125@126.com>
2021-01-11 18:01:33 +0800

    finished crud application

--- react-node-mysql/client/src/App.js
+++ react-node-mysql/client/src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [movieName, setMovieName] = useState("");
   const [review, setReview] = useState("");
   const [movieReviewList, setMovieReviewList] = useState([]);
+  const [newReview, setNewReview] = useState("");
 
   useEffect(() => {
     Axios.get("http://localhost:3001/api/get").then((res) => {
@@ -17,8 +18,20 @@ function App() {
     Axios.post("http://localhost:3001/api/insert", {
       movieName: movieName,
       movieReview: review,
-    }).then(() => {
-      alert("successful insert");
+    }).then((err, ret) => {});
+    setMovieReviewList([
+      ...movieReviewList,
+      { movieName: movieName, movieReview: review },
+    ]);
+  };
+  const deleteReview = (movie) => {
+    Axios.delete(`http://localhost:3001/api/delete/${movie}`);
+  };
+
+  const updateReview = (movie) => {
+    Axios.put("http://localhost:3001/api/update", {
+      movieName: movie,
+      movieReview: newReview,
     });
   };
 
@@ -45,9 +58,23 @@ function App() {
         <button onClick={submitReview}>Submit</button>
         {movieReviewList.map((item) => {
           return (
-            <h4>
-              MovieName: {item.movieName} | MovieReview: {item.movieReview}
-            </h4>
+            <div className="card">
+              <h1>{item.movieName}</h1>
+              <p>{item.movieReview}</p>
+              <button onClick={() => deleteReview(item.movieName)}>
+                Delete
+              </button>
+              <input
+                type="text"
+                id="updateInput"
+                onChange={(e) => {
+                  setNewReview(e.target.value);
+                }}
+              />
+              <button onClick={() => updateReview(item.movieName)}>
+                Update
+              </button>
+            </div>
           );
         })}
       </div>
--- react-node-mysql/server/index.js
+++ react-node-mysql/server/index.js
@@ -31,6 +31,23 @@ app.get("/api/get", (req, res) => {
   });
 });
 
+app.delete("/api/delete/:movieName", (req, res) => {
+  const name = req.params.movieName;
+  const sqlDelete = "delete from movie_reviews where movieName = ?";
+  db.query(sqlDelete, name, (err, ret) => {
+    if (err) console.log(err);
+  });
+});
+app.put("/api/update", (req, res) => {
+  const name = req.body.movieName;
+  const review = req.body.movieReview;
+  const sqlUpdate =
+    "update movie_reviews set movieReview = ? where movieName = ?";
+  db.query(sqlUpdate, [review, name], (err, ret) => {
+    if (err) console.log(err);
+  });
+});
+
 app.post("/api/insert", (req, res) => {
   const movieName = req.body.movieName;
   const movieReview = req.body.movieReview;
